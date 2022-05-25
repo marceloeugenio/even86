3094f54f8979b43a4f047e28867eb1a388913717
Marcelo Eugênio <marcelo.eugenio@gmail.com>
2022-05-25 08:37:07 -0300

    Update

--- even86/settings.py
+++ even86/settings.py
@@ -91,20 +91,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'even86.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
