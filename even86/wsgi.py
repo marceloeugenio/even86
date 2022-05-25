79ac11533b09819a77bdf8ccf4fa3cc07937d3dd
Marcelo Eugênio <marcelo.eugenio@gmail.com>
2022-05-25 08:31:05 -0300

    Update

--- even86/wsgi.py
+++ even86/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for even86 project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
 
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "even86.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'even86.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
